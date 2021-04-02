16e426019afd8a0430710ac12c84fa093cac486e
Green-Avocado <greenavocado@protonmail.com>
2021-04-01 17:46:41 -0700

    fix: typo

--- pwndocker.py
+++ pwndocker.py
@@ -25,8 +25,8 @@ ld = options.ld
 runstring = ""
 
 if libc and ld:
-    runstring += " ln -s {} /lib/x64_64-linux-gnu/libc.so.6;".format(libc)
-    runstring += " ln -s {} /lib/x64_64-linux-gnu/ld-linux-x86-64.so.2;".format(ld)
+    runstring += " ln -s {} /lib/x86_64-linux-gnu/libc.so.6;".format(libc)
+    runstring += " ln -s {} /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2;".format(ld)
 elif libc or ld:
     parser.error("libc and ld must be provided together")
     exit(1)
