fd4f397fcb6bc0b36c936f13b6c6ad4ee8388905
Green-Avocado <greenavocado@protonmail.com>
2021-04-02 19:46:02 -0700

    feat: run gdbserver

--- pwndocker.py
+++ pwndocker.py
@@ -31,18 +31,20 @@ ld = options.ld
 runstring = ""
 
 if libc and ld:
-    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/libc.so.6;".format(libc)
-    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/ld-2.19.so;".format(ld)
+    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/libc.so.6; ".format(libc)
+    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/ld-2.19.so; ".format(ld)
 elif libc or ld:
     parser.error("libc and ld must be provided together")
     exit(1)
 
-runstring += "socat TCP-LISTEN:1337,fork,reuseaddr EXEC:'./{}';".format(binary)
+runstring += "gdbserver --multi localhost:13337 & "
+runstring += "socat TCP-LISTEN:1337,fork,reuseaddr EXEC:'./{}'; ".format(binary)
 
 cmd = ["docker", "run", "--rm"]
 cmd.extend(["--name", "pwndocker"])
 cmd.extend(["--mount", "type=bind,source={},target=/mnt,readonly".format(os.path.abspath('.'))])
 cmd.extend(["--publish", "1337:1337/tcp"])
+cmd.extend(["--publish", "13337:13337/tcp"])
 cmd.append("pwndocker")
 cmd.extend(["/bin/bash", "-c", runstring])
 
