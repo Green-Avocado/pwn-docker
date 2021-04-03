99aeb45b6e1661090e12b7bac1c4895b3d0aadcc
Green-Avocado <greenavocado@protonmail.com>
2021-04-02 17:02:05 -0700

    feat: use pwd for ln-static src path

--- pwndocker.py
+++ pwndocker.py
@@ -30,8 +30,8 @@ ld = options.ld
 runstring = ""
 
 if libc and ld:
-    runstring += "/tmp/ln-static /mnt/{} /lib/x86_64-linux-gnu/libc.so.6;".format(libc)
-    runstring += "/tmp/ln-static /mnt/{} /lib/x86_64-linux-gnu/ld-2.19.so;".format(ld)
+    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/libc.so.6;".format(libc)
+    runstring += "/tmp/ln-static $(pwd)/{} /lib/x86_64-linux-gnu/ld-2.19.so;".format(ld)
 elif libc or ld:
     parser.error("libc and ld must be provided together")
     exit(1)
