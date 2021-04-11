de93de43a8adde6d6e731f8f09ece078151db089
Green-Avocado <greenavocado@protonmail.com>
2021-04-11 14:10:09 -0700

    feat: replace mkdir step with -D install flag

--- setup.sh
+++ setup.sh
@@ -5,6 +5,5 @@ gcc ln-static.c -o ln-static --static
 sudo docker image rm pwndocker
 sudo docker build -t pwndocker .
 
-mkdir -p ~/bin
-install pwndocker.py ~/bin/pwndocker
+install -D pwndocker.py ~/bin/pwndocker
 
