b3750bf9897c6ed02d7080c1390eeca991b7e1c1
Green-Avocado <greenavocado@protonmail.com>
2021-04-15 15:22:54 -0700

    feat: updated glibc-fetch

--- glibc-fetch.py
+++ glibc-fetch.py
@@ -2,6 +2,7 @@
 
 import requests
 import re
+import subprocess
 from optparse import OptionParser
 
 usage = "usage: %prog LIBC"
@@ -11,11 +12,10 @@ parser.add_option(
         action="store_true", dest="version",
         help="display version information and exit"
         )
-
 (options, args) = parser.parse_args()
 
 if options.version:
-    print("glibc-fetch 1.0.1")
+    print("glibc-fetch 1.1.0")
     exit()
 
 if len(args) == 0:
@@ -28,11 +28,19 @@ libc_filename = args[0]
 try:
     libc = open(libc_filename, 'rb').read()
     libc_ver = re.search(b'GNU C Library \(Ubuntu GLIBC (.+?)\)', libc).group(1).decode()
+
+    fileinfo = subprocess.check_output(['file', '-b', libc_filename]).split()[1].decode()
+    if fileinfo == "64-bit":
+        arch = ""
+    elif fileinfo == "32-bit":
+        arch = "-1386"
+    else:
+        raise
 except:
     print("fatal error")
     exit()
 
-url = "https://launchpad.net/ubuntu/+archive/primary/+files/libc6_{}_amd64.deb".format(libc_ver)
+url = "https://launchpad.net/ubuntu/+archive/primary/+files/libc6{}_{}_amd64.deb".format(arch, libc_ver)
 
 print(url)
 
