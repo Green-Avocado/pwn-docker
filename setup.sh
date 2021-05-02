bb9f708297a495cec9ac6389fd7b594c5c1c6bdb
Green-Avocado <greenavocado@protonmail.com>
2021-05-02 03:24:09 -0700

    feat: sorted files into dirs

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-gcc ln-static.c -o ln-static --static
+gcc ln-static.c -o ./docker/ln-static --static
 
 sudo docker image rm pwndocker
-sudo docker build -t pwndocker .
+sudo docker build -t pwndocker ./docker
 
-install -D pwndocker.py ~/bin/pwndocker
-install -D glibc-fetch.py ~/bin/glibc-fetch
+install -D ./src/pwndocker.py ~/bin/pwndocker
+install -D ./src/glibc-fetch.py ~/bin/glibc-fetch
 
