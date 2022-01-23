258d4cb1374b6964251ee72cd2c2f0de7165d6be
Green-Avocado <greenavocado@protonmail.com>
2022-01-23 00:34:12 -0800

    move new files first, then overwrite

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,8 +12,13 @@ fi
 
 if [ -n "$2" ]; then
     dpkg-deb -R "$2" /tmp
-    mv /tmp/lib/x86_64-linux-gnu/* /lib/x86_64-linux-gnu/
-    mv /tmp/lib32/* /lib/i386-linux-gnu/
+
+    mv -n /tmp/lib/x86_64-linux-gnu/* /lib/x86_64-linux-gnu/
+    mv -f /tmp/lib/x86_64-linux-gnu/* /lib/x86_64-linux-gnu/
+
+    mv -n /tmp/lib32/* /lib/i386-linux-gnu/
+    mv -f /tmp/lib32/* /lib/i386-linux-gnu/
+
     /tmp/ln-static /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2
     /tmp/ln-static /lib/i386-linux-gnu/ld-linux.so.2 /lib/ld-linux.so.2
 fi
